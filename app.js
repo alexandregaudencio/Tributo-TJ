3d3628395585906f0605cbd4fa5ad194d0df550f
Alexandre Gaudêncio <alexandre.gtpjr@gmail.com>
2021-08-31 21:17:10 -0300

    Timer added

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
+// const timer = ;
+
 var trilha = new Audio("./song/ipanema.mp3");
 window.onload = function() {
     PlaySong();
+    StartTimer();
 }
 
 
 function PlaySong() {
     trilha.play();
+
 }
+
+function AddZero(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+}
+
+function StartTimer() {
+    setInterval(() => {
+        var d = new Date();
+        var h = AddZero(d.getUTCHours());
+        var m = AddZero(d.getUTCMinutes());
+        var s = AddZero(d.getUTCSeconds());
+        document.querySelector("#timer").innerHTML = h + ":" + m + ":" + s;
+        console.log(h);
+        
+    }, 1000);
+}
+
+
+
